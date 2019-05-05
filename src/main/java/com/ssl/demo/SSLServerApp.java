6921e7daf092c3daa66f2513af931da4bebe8743
cognipraj <Pushparaj@cognissolutions.com>
2019-05-05 13:16:09 +0530

    initil draft

---
+++ src/main/java/com/ssl/demo/SSLServerApp.java
@@ -0,0 +1,12 @@
+package com.ssl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSLServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSLServerApp.class, args);
+	}
+}
